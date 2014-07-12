86bd5ca7f5b98a34a12b3b1997a9f84d1e207379
thesamovar <dan.goodman@ens.fr>
2014-07-12 00:52:14 -0400

    Should do more or less what the old brian2 genn device did now, but not sure if it is correct or will work in genn

--- brian2genn/device.py
+++ brian2genn/device.py
@@ -4,11 +4,14 @@ import inspect
 from collections import defaultdict
 
 from brian2.units import second
+from brian2.codegen.generators.cpp_generator import c_data_type
 from brian2.core.clocks import defaultclock
-from brian2.devices.device import Device, set_device, all_devices
-from brian2.devices.cpp_standalone.device import CPPStandaloneDevice
 from brian2.core.preferences import brian_prefs
 from brian2.core.variables import *
+from brian2.core.network import Network
+from brian2.devices.device import Device, set_device, all_devices
+from brian2.devices.cpp_standalone.device import CPPStandaloneDevice
+from brian2.synapses.synapses import Synapses
 from brian2.utils.filetools import copy_directory, ensure_directory, in_directory
 from brian2.utils.stringtools import word_substitute
 from brian2.memory.dynamicarray import DynamicArray, DynamicArray1D
@@ -26,6 +29,7 @@ def decorate(code, variables, parameters):
         code = word_substitute(code, {p : '$('+p+')'})
     code= word_substitute(code, {'dt' : 'DT'});
     code= code.replace('\n', '\\n\\\n')
+    code = code.replace('"', '\\"')
     return code
 
 class neuronModel(object):
@@ -50,118 +54,85 @@ class GeNNDevice(CPPStandaloneDevice):
 
     def build(self, project_dir='output'):
 
+        # Check for GeNN compatibility
+        
         if len(self.dynamic_arrays) or len(self.dynamic_arrays_2d):
             raise NotImplementedError("GeNN does not support objects that use dynamic arrays (Synapses, SpikeMonitor, etc.)")
+                
+        networks = [net() for net in Network.__instances__() if net().name!='_fake_network']
+        synapses = [S() for S in Synapses.__instances__()]
         
+        if len(synapses):
+            raise NotImplementedError("GeNN does not support Synapses (yet).")
+        
+        if len(networks)!=1:
+            raise NotImplementedError("GeNN only supports a signle Network object")
+        net = networks[0]
+
+        # Start building the project
+
         self.project_dir = project_dir
-        ensure_directory(project_dir)        
+        ensure_directory(project_dir)
         
-        raise NotImplementedError
-    
-        ######### WHAT FOLLOWS IS THE GENN CODE THAT THOMAS NOWOTNY WROTE IN 2013
-        ######### This needs to be updated to work with the changes in Brian's standalone design
-        ######### To do so, I derived the class from CPPStandaloneDevice so that we only need to rewrite the
-        ######### build() method, following the way it is done in CPPStandaloneDevice.build.
-       
+        objects = dict((obj.name, obj) for obj in net.objects)
+        neuron_groups = [obj for obj in net.objects if isinstance(obj, NeuronGroup)]
+        
         # assemble the model descriptions:
         self.model_name= net.name+'_model'
-        for obj in net.objects:
+        for obj in neuron_groups:
             # Extract the variables
-            if isinstance(obj, NeuronGroup):
-#                print 'we have a neuron group'
-                neuron_model= neuronModel();
-                neuron_model.name= obj.name
-                neuron_model.N= obj.N
-                for k, v in obj.variables.iteritems():
-                    if k == '_spikespace' or k == 't' or k == 'dt':
-                        pass
-                    else:
-                        neuron_model.variables.append((k, c_data_type(v.dtype)))
-       
-                trgnm= obj.name+'_stateupdater'
-                for obj2 in net.objects:
-                    if obj2.name == trgnm:
-                        updater= obj2.updaters[0]
-                        codeobj= updater.owner
-                        for k, v in codeobj.namespace.iteritems():
-                            if k == 'dt':
-                                self.dtDef= '#define DT '+repr(v)
-                            elif isinstance(v, (int, float)):
-                                if k not in neuron_model.parameters:
-                                    neuron_model.parameters.append(k)
-                                    neuron_model.pvalue.append(repr(v)) 
-                              
-                        code = decorate(codeobj.code.cpp_file, neuron_model.variables, neuron_model.parameters)
-                        neuron_model.code_lines.append(code)                    
-                trgnm= obj.name+'_thresholder'
-                for obj2 in net.objects:
-                    if obj2.name == trgnm:
-                        for updater in obj2.updaters:
-                            codeobj= updater.owner
-                            for k, v in codeobj.namespace.iteritems():
-                                if k == 'dt':
-                                    self.dtDef= '#define DT '+repr(v)
-                                elif isinstance(v, (int, float)):
-                                    if k not in neuron_model.parameters:
-                                        neuron_model.parameters.append(k)
-                                        neuron_model.pvalue.append(repr(v)) 
-                            code = decorate(codeobj.code.cpp_file, neuron_model.variables, neuron_model.parameters)
-                            print code
-                            neuron_model.thresh_cond_lines.append(code)  
-                trgnm= obj.name+'_resetter'
-                for obj2 in net.objects:
-                    if obj2.name == trgnm:
-                        for updater in obj2.updaters:
-                            codeobj= updater.owner
-                            for k, v in codeobj.namespace.iteritems():
-                                if k == 'dt':
-                                    self.dtDef= '#define DT '+repr(v)
-                                elif isinstance(v, (int, float)):
-                                    if k not in neuron_model.parameters:
-                                        neuron_model.parameters.append(k)
-                                        neuron_model.pvalue.append(repr(v)) 
-                            code = decorate(codeobj.code.cpp_file, neuron_model.variables, neuron_model.parameters)
-                            neuron_model.reset_code_lines.append(code)  
-                
-#                if (hasattr(obj, 'threshold')):
-#                    if (obj.threshold <> None):
-#                        code = decorate(obj.threshold, neuron_model.variables, neuron_model.parameters)
-#                        neuron_model.thresh_cond_lines.append(code)
-                self.neuron_models.append(neuron_model)
+            neuron_model= neuronModel();
+            neuron_model.name= obj.name
+            neuron_model.N= obj.N
+            for k, v in obj.variables.iteritems():
+                if k == '_spikespace' or k == 't' or k == 'dt':
+                    pass
+                elif isinstance(v, ArrayVariable):
+                    neuron_model.variables.append((k, c_data_type(v.dtype)))
+   
+            for suffix, lines in [('_stateupdater', neuron_model.code_lines),
+                                  ('_thresholder', neuron_model.thresh_cond_lines),
+                                  ('_resetter', neuron_model.reset_code_lines),
+                                  ]:
+                codeobj = objects[obj.name+suffix].codeobj
+                for k, v in codeobj.variables.iteritems():
+                    if k == 'dt':
+                        self.dtDef= '#define DT '+repr(getattr(v.obj, v.attribute))
+                    elif isinstance(v, Constant):
+                        if k not in neuron_model.parameters:
+                            neuron_model.parameters.append(k)
+                            neuron_model.pvalue.append(repr(v.value)) 
+                      
+                code = decorate(codeobj.code.cpp_file, neuron_model.variables, neuron_model.parameters)
+                lines.append(code)                    
+            
+            self.neuron_models.append(neuron_model)
         
-        # The code_objects are passed in the right order to run them because they were
-        # sorted by the Network object. To support multiple clocks we'll need to be
-        # smarter about that.
-        print self.neuron_models
-        #print self.neuron_models[2].code_lines
-        model_tmp = GeNNCodeObject.templater.model(None,
+        model_tmp = GeNNCodeObject.templater.model(None, None,
                                                    neuron_models= self.neuron_models,
                                                    dtDef= self.dtDef,
                                                    model_name= self.model_name,
                                                    )
         open('output/'+self.model_name+'.cc', 'w').write(model_tmp)
 
-        runner_tmp = GeNNCodeObject.templater.runner(None,
+        runner_tmp = GeNNCodeObject.templater.runner(None, None,
                                                      neuron_models= self.neuron_models,
                                                      model_name= self.model_name,
-                                                                  )        
+                                                     )        
         open('output/runner.cu', 'w').write(runner_tmp.cpp_file)
         open('output/runner.h', 'w').write(runner_tmp.h_file)
-        engine_tmp = GeNNCodeObject.templater.engine(None,
+        engine_tmp = GeNNCodeObject.templater.engine(None, None,
                                                      neuron_models= self.neuron_models,
-                                                     model_name= self.model_name,                                                              )        
+                                                     model_name= self.model_name,
+                                                     )        
         open('output/engine.cc', 'w').write(engine_tmp.cpp_file)
         open('output/engine.h', 'w').write(engine_tmp.h_file) 
 
-        Makefile_tmp= GeNNCodeObject.templater.Makefile(None,
+        Makefile_tmp= GeNNCodeObject.templater.Makefile(None, None,
                                                         neuron_models= self.neuron_models,
-                                                        model_name= self.model_name,                                                              ) 
+                                                        model_name= self.model_name,
+                                                        ) 
         open('output/Makefile', 'w').write(Makefile_tmp);
-
-        # Copy the brianlibdirectory
-        brianlib_dir = os.path.join(os.path.split(inspect.getsourcefile(GeNNCodeObject))[0],
-                                    'brianlib')
-        copy_directory(brianlib_dir, 'output/brianlib')
 
 
 genn_device = GeNNDevice()
