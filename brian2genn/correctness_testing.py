f95ba97ea261d062380e79a2f8849573370d5f5d
thesamovar <dan.goodman@ens.fr>
2014-10-30 11:43:58 +0000

    minor simplification

--- brian2genn/correctness_testing.py
+++ brian2genn/correctness_testing.py
@@ -2,14 +2,13 @@ import brian2
 import os
 import shutil
 import sys
-from StringIO import StringIO
 from brian2.tests.features import (Configuration, DefaultConfiguration,
                                    run_feature_tests)
 
 class GeNNConfiguration(Configuration):
     name = 'GeNN'
     def before_run(self):
-        brian2.prefs.read_preference_file(StringIO(brian2.prefs.defaults_as_file))
+        brian2.prefs.reset_to_defaults()
         brian2.set_device('genn')
         
     def after_run(self):
