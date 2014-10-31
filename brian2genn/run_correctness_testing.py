2ecf8a89c5983377b43c691b6316a4f071341262
thesamovar <dan.goodman@ens.fr>
2014-10-31 00:51:11 +0000

    Updated run_correctness_testing

--- brian2genn/run_correctness_testing.py
+++ brian2genn/run_correctness_testing.py
@@ -3,11 +3,16 @@ from brian2.tests.features import (Configuration, DefaultConfiguration,
                                    run_feature_tests, run_single_feature_test)
 from brian2genn.correctness_testing import GeNNConfiguration
 
-from brian2.tests.features.synapses import SynapsesPre
+from brian2.tests.features.synapses import SynapsesPre, SynapsesPost
 #c = GeNNConfiguration()
 #c.before_run()
 #f = SynapsesPre()
 #f.run()
 #c.after_run()
-#run_single_feature_test(GeNNConfiguration, SynapsesPre)
-print run_feature_tests(configurations=[DefaultConfiguration, GeNNConfiguration])
+#run_single_feature_test(GeNNConfiguration, SynapsesPre).tables_and_exceptions
+#print run_feature_tests(configurations=[DefaultConfiguration,
+#                                        GeNNConfiguration],
+#                        feature_tests=[SynapsesPre,
+#                                       SynapsesPost]).tables_and_exceptions
+print run_feature_tests(configurations=[DefaultConfiguration,
+                                    GeNNConfiguration]).tables_and_exceptions
