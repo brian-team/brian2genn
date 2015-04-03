98236041804e86ef0caecce4f59caeccb156d691
Thomas Nowotny <t.nowotny@sussex.ac.uk>
2015-04-03 21:46:24 +0100

    Started to work on a SPikeGeneratorGroup implementation. So far only copied the test to have a starting point to see how it works.

---
+++ examples/simple_spikesource.py
@@ -0,0 +1,24 @@
+from brian2 import *
+import brian2genn
+
+set_device('genn')
+#set_device('cpp_standalone')
+
+G = NeuronGroup(10, 'v:1')
+mon = StateMonitor(G, 'v', record=True)
+indices = np.array([3, 2, 1, 1, 4, 5])
+times =   np.array([6, 5, 4, 3, 3, 1]) * ms
+SG = SpikeGeneratorGroup(10, indices, times)
+S = Synapses(SG, G, pre='v+=1', connect='i==j')
+net = Network(G, SG, mon, S)
+net.run(7*ms)
+
+device.build(directory='simple_spikesource',
+             compile=True,
+             run=True,
+             use_GPU=True)
+
+#device.build(directory='simple_statemon',
+#             compile=True,
+#             run=True)
+
