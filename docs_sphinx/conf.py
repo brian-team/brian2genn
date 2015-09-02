fe4dee5d8b0390d601ff8618c0ff25ffe9bd9fc4
Thomas Nowotny <t.nowotny@sussex.ac.uk>
2015-09-02 19:58:41 +0100

    Modified sphinc config removing the extension sphinxcontrib.issuetracker

--- docs_sphinx/conf.py
+++ docs_sphinx/conf.py
@@ -79,7 +79,8 @@ extensions = ['sphinx.ext.autodoc', 'sphinx.ext.doctest',
               'sphinx.ext.viewcode',
 #              'brian2.sphinxext.briandoc',
               'sphinx.ext.autosummary',
-              'sphinxcontrib.issuetracker']
+#              'sphinxcontrib.issuetracker'
+]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
