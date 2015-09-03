24f6f2ab1dc4351e6956956d3d5f6b77b485f2b9
Thomas Nowotny <t.nowotny@sussex.ac.uk>
2015-09-03 16:50:19 +0100

    Minor adjustment in docs

--- docs_sphinx/conf.py
+++ docs_sphinx/conf.py
@@ -123,7 +123,7 @@ release = '0.9b1+git'
 exclude_patterns = ['_build']
 
 # The reST default role (used for this markup: `text`) to use for all documents.
-default_role = 'object'
+#default_role = 'brianobj'
 
 # If true, '()' will be appended to :func: etc. cross-reference text.
 #add_function_parentheses = True
--- docs_sphinx/index.rst
+++ docs_sphinx/index.rst
@@ -5,6 +5,7 @@ Contents:
 
 .. toctree::
    :maxdepth: 2
+   :titlesonly:
 
    introduction/index
    introduction/exclusions
