b24bfbcaf6a292d24799f090ffbacc27b156d005
Thomas Nowotny <t.nowotny@sussex.ac.uk>
2015-09-03 15:40:42 +0100

    lso removed brian2genn.sphinxext.briandoc

--- docs_sphinx/conf.py
+++ docs_sphinx/conf.py
@@ -78,7 +78,7 @@ extensions = ['sphinx.ext.autodoc', 'sphinx.ext.doctest',
               'sphinx.ext.intersphinx', 'sphinx.ext.todo',
               'sphinx.ext.coverage', 'sphinx.ext.mathjax',
               'sphinx.ext.viewcode',
-              'brian2genn.sphinxext.briandoc',
+#              'brian2genn.sphinxext.briandoc',
               'sphinx.ext.autosummary',
 #              'sphinxcontrib.issuetracker'
 ]
