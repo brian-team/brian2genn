4f0de4d93d0d6b163cf2e8b175b9a6582cf59bde
Marcel Stimberg <marcel.stimberg@inserm.fr>
2016-11-10 16:31:07 +0100

    Make the "run_brian_tests" scrips fail with an error code in case of errors

--- scripts/run_brian_tests.py
+++ scripts/run_brian_tests.py
@@ -1,16 +1,11 @@
+import sys
+
 import brian2genn
 import brian2
-from brian2.tests.features import (Configuration, DefaultConfiguration,
-                                   run_feature_tests, run_single_feature_test)
-from brian2genn.correctness_testing import GeNNConfiguration
-from brian2.tests.features.synapses import *
-from brian2.tests.features.neurongroup import *
-from brian2.tests.features.monitors import *
-from brian2.tests.features.speed import *
-from brian2.tests.features.input import *
-from brian2.tests.features import CPPStandaloneConfiguration
-from brian2 import prefs
 
-if __name__=='__main__':
-    brian2.test([], test_codegen_independent=False, test_standalone='genn',
-                fail_for_not_implemented=False)
+if __name__ == '__main__':
+    success = brian2.test([], test_codegen_independent=False,
+                          test_standalone='genn',
+                          fail_for_not_implemented=False)
+    if not success:
+        sys.exit(1)
