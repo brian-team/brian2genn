4ce55e0e8d9e6fac748a4ea391ca32ce07548a91
Marcel Stimberg <marcel.stimberg@inserm.fr>
2017-05-30 15:38:03 +0200

    ...and correct the SHA256 checksum as well (OS X)

--- cuda_toolkit.rb
+++ cuda_toolkit.rb
@@ -1,6 +1,6 @@
 cask 'cuda_toolkit' do
   version '8.0.61'
-  sha256 'b70a193cbe0a798d7363abab88ab5190409d237d7e13bf8682682fbbeac01847'
+  sha256 '8a47dd19108c1f5ddb0f3bf6a23d5ad29598e5985eee8bebe2febd91b2796ee9'
 
   url "https://developer.nvidia.com/compute/cuda/#{version.major_minor}/Prod2/network_installers/cuda_#{version}_mac_network-dmg"
   name 'Nvidia CUDA'
