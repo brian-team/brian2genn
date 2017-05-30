8a481b090b60d3f8f1203f67dbedada68a651e65
Marcel Stimberg <marcel.stimberg@inserm.fr>
2017-05-30 16:46:59 +0200

    Fix syntax issues in bld.bat

--- conda_recipe/bld.bat
+++ conda_recipe/bld.bat
@@ -1,7 +1,7 @@
-# Compile GeNN libraries
-set GENN_PATH=%CD%/genn
-nmake /f genn/lib/WINmakefile %GENN_PATH%/lib/lib/genn.lib "GENN_PATH=%GENN_PATH%"
-nmake /f genn/lib/WINmakefile %GENN_PATH%/lib/lib/genn_CPU_ONLY.lib "GENN_PATH=%GENN_PATH%" CPU_ONLY=1
+REM Compile GeNN libraries
+set GENN_PATH=%CD%\genn
+nmake /f genn\lib\WINmakefile %GENN_PATH%\lib\lib\genn.lib "GENN_PATH=%GENN_PATH%"
+nmake /f genn\lib\WINmakefile %GENN_PATH%\lib\lib\genn_CPU_ONLY.lib "GENN_PATH=%GENN_PATH%" CPU_ONLY=1
 
-# Install Python package with GeNN libraries
+REM Install Python package with GeNN libraries
 python setup.py install --single-version-externally-managed --record record.txt --with-genn
