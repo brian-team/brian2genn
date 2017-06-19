47c8a302b9e79d6e4dafd2bbef0f86f3aa1bef01
Marcel Stimberg <marcel.stimberg@inserm.fr>
2017-06-19 14:04:43 +0200

    Avoid conflicts between installed GeNN and GENN_PATH by unsetting GENN_PATH

--- brian2genn/device.py
+++ brian2genn/device.py
@@ -856,6 +856,11 @@ class GeNNDevice(CPPStandaloneDevice):
             genn_path = os.path.join(sys.prefix, 'opt', 'genn')
             logger.debug('Using GeNN path from installation: '
                          '"{}"'.format(genn_path))
+            if 'GENN_PATH' in os.environ:
+                logger.debug('Unsetting the GENN_PATH environment variable to '
+                             'avoid conflicts with the installed GeNN version.')
+                os.unsetenv('GENN_PATH')
+                del os.environ['GENN_PATH']
         elif 'GENN_PATH' in os.environ:
             genn_path = os.environ['GENN_PATH']
             logger.debug('Using GeNN path from environment variable: '
