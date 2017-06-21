b59b8d16ca47a370a5de0e1e50f1767e40729a16
Marcel Stimberg <marcel.stimberg@inserm.fr>
2017-06-21 18:52:37 +0200

    Try to make the conda build script more robust

--- scripts/build_conda_packages.py
+++ scripts/build_conda_packages.py
@@ -16,10 +16,17 @@ recipe_path = os.path.join(os.path.dirname(__file__), '..', 'conda_recipe')
 python_versions = ['2.7', '3.5', '3.6']
 additional_args = sys.argv[1:]
 
+# Run conda build purge first
+sys.argv = ['conda-build', 'purge']
+main_build()
+
+# Call conda build for each python version
 for python_version in python_versions:
     shutil.rmtree(os.path.join(recipe_path, '..', 'build'), ignore_errors=True)
     sys.argv = ['conda-build', '-c', 'brian-team', '--quiet',
-                '--python={}'.format(python_version)]
+                '--python={}'.format(python_version),
+                '--no-activate'  # seems to make the build more robust on Windows
+                ]
     sys.argv.extend(additional_args)
     sys.argv.append(recipe_path)
     main_build()
