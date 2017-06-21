cc2afa26ae57f86439b6c2ef50b89a14279ea753
Marcel Stimberg <marcel.stimberg@inserm.fr>
2017-06-21 19:08:52 +0200

    ***** Release brian2genn 1.0rc5 *****

--- setup.py
+++ setup.py
@@ -50,7 +50,7 @@ except ValueError:
     data_files = []
 
 setup(name='Brian2GeNN',
-      version='1.0rc4',
+      version='1.0rc5',
       packages=find_packages(),
       package_data={# include template files
                     'brian2genn': ['templates/*.cpp',
