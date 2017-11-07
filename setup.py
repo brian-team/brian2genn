94aa5e58bc1c740faaee9e2051c2ab98eba8b6bb
Marcel Stimberg <marcel.stimberg@inserm.fr>
2017-11-07 15:00:09 +0100

    Set version to 1.1.5+git
    [ci skip]

--- setup.py
+++ setup.py
@@ -50,7 +50,7 @@ except ValueError:
     data_files = []
 
 setup(name='Brian2GeNN',
-      version='1.1.5',
+      version='1.1.5+git',
       packages=find_packages(),
       package_data={# include template files
                     'brian2genn': ['templates/*.cpp',
