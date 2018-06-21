8602313d3509d20cf67a04326067d5c34158439c
Thomas Nowotny <tn41@inf900777.inf.susx.ac.uk>
2018-06-21 19:18:20 +0100

    Small changes to the benchmarking scripts

--- scripts/benchmarking/run_benchmarks_genn.sh
+++ scripts/benchmarking/run_benchmarks_genn.sh
@@ -1,26 +1,26 @@
 #!/bin/bash -e
 
 for spikemon in "true"; do
-   for threads in 1 2 4 8 16; do
-       for scaling in  0.05 0.1 0.25 0.5 1 2 4 8 16 32; do
+   for threads in 0 -1; do
+       for scaling in 0.05 0.1 0.25 0.5 1 2 4 8 16 32; do
 	   time=1;
                for repeat in 1 2 3; do
                    echo Repeat $repeat
-                   python $1 $scaling cpp_standalone $threads $spikemon true $time
-                   rm -r output
+                   python $1 $scaling genn $threads $spikemon true $time
+                   rm -r GeNNworkspace
 	       done
        done
    done
 done
 		   
 # The really long runs (don't run with GeNN CPU-only, etc.)
-for spikemon in "true" "false"; do
+for spikemon in "true"; do
     for scaling in 64 128 256; do
 	time=1;
 	    for repeat in 1 2; do
 		echo Repeat $repeat
-                python $1 $scaling cpp_standalone 16 $spikemon true $time
-		rm -r output
+                python $1 $scaling genn 0 $spikemon true $time
+                rm -r GeNNworkspace
 	    done
     done
 done
