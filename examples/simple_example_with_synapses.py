f6ea0ac2b4ab8c98b0cca7bbf51f9bcadb4abf75
Thomas Nowotny <t.nowotny@sussex.ac.uk>
2020-04-22 16:26:27 +0100

    Update examples/simple_example_with_synapses.py
    Co-Authored-By: Marcel Stimberg <marcel.stimberg@inserm.fr>

--- examples/simple_example_with_synapses.py
+++ examples/simple_example_with_synapses.py
@@ -13,8 +13,8 @@ dV/dt = -V/tau + Iin : 1
 G = NeuronGroup(Npre, eqs, threshold='V>1', reset='V=0', refractory=5 * ms)
 G2= NeuronGroup(Npost, eqs, threshold='V>1', reset='V=0', refractory=5 * ms)
 
-S=Synapses(G, G2, 'w:1', on_pre='V+= w');
-S2= Synapses(G2, G, 'w:1', on_pre='V+= w');
-S.connect(i=np.array([1, 3, 4, 2]), j=np.array([2, 2, 2, 2]));
-S2.connect(condition= 'i != j',p=0.5);
+S =Synapses(G, G2, 'w:1', on_pre='V+= w')
+S2 = Synapses(G2, G, 'w:1', on_pre='V+= w')
+S.connect(i=np.array([1, 3, 4, 2]), j=np.array([2, 2, 2, 2]))
+S2.connect(condition='i != j', p=0.5)
 run(10*ms)
